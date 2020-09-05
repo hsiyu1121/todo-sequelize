d67b4219e31a195f3ff03dadea9a3e79646b0043
hsiyu1121 <bluefish1121@gmail.com>
2020-09-05 22:11:46 +0800

    feat: final

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const flash = require("connect-flash");
-
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config()
-}
 
 const routes = require("./routes");
 const app = express();
@@ -21,7 +20,7 @@ app.use(methodOverride("_method"));
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || 'ThisIsMySecret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
@@ -33,48 +32,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.success_msg = req.flash("success_msg");
   res.locals.warning_msg = req.flash("warning_msg");
-});
-
-app.use(routes);
-
-app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`);
-});
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const flash = require("connect-flash");
-
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config()
-}
-
-const routes = require("./routes");
-const app = express();
-const usePassport = require("./config/passport");
-const PORT = 3000
-
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", "hbs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || 'ThisIsMySecret',
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-usePassport(app);
-app.use(flash());
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated();
-  res.locals.user = req.user;
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.warning_msg = req.flash("warning_msg");
+  next()
 });
 
 app.use(routes);
